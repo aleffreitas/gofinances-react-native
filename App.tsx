4ac27b5aefe7e2507d3bbf9691f5cbbecefbc062
aleffreitas <alef.freitas07@gmail.com>
2023-03-09 16:31:56 -0300

    chore: added fonts and SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
 import theme from './src/global/styles/theme';
 
 import { DashBoard } from './src/pages/Dashboard';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <DashBoard />
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,13 +1,18 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.background};
+  `}
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
 
 export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.title};
+    font-family: ${theme.fonts.bold};
+  `}
   font-size: 24px;
-  font-weight: bold;
-  color: purple;
 `;
